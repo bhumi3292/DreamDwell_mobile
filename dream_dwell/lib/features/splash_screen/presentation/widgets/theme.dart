0b68ff5f9c5ffb5092794ad032af7b880377af73
bhumi3292 <137974891+bhumi3292@users.noreply.github.com>
2025-05-30 20:53:11 +0545

    theme

---
+++ dream_dwell/lib/features/splash_screen/presentation/widgets/theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+const Color navyBlue = Color(0xFF003366);
+
+ThemeData getApplication() {
+  return ThemeData(
+    useMaterial3: false,
+    primaryColor: navyBlue,
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: navyBlue,
+        elevation: 5,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+    ),
+  );
+}
