036aea63056395bf5c4cc69e785103b034975482
bhumi3292 <137974891+bhumi3292@users.noreply.github.com>
2025-05-30 21:01:07 +0545

    booking

---
+++ dream_dwell/lib/view/booking.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:dream_dwell/features/splash_screen/presentation/widgets/header_nav.dart';
+import 'package:dream_dwell/features/splash_screen/presentation/widgets/nav_bar.dart';
+
+class BookingPage extends StatelessWidget {
+  const BookingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: const Center(
+          child: Text(
+            "Welcome to the booking",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 34,
+              fontWeight: FontWeight.bold,
+              color: Color(0xFF003366),
+            ),
+          ),
+        ),
+       
+      ),
+    );
+  }
+}
