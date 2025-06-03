c9252cf7dce6b75473f398f1581f4dcec655931b
bhumi3292 <137974891+bhumi3292@users.noreply.github.com>
2025-06-03 13:57:32 +0545

    Explore Search

--- dream_dwell/lib/view/explore.dart
+++ dream_dwell/lib/view/explore.dart
@@ -1,23 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:dream_dwell/features/splash_screen/presentation/widgets/header_nav.dart';
-import 'package:dream_dwell/features/splash_screen/presentation/widgets/nav_bar.dart';
+
 
 class ExplorePage extends StatelessWidget {
   const ExplorePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
-        body: const Center(
-          child: Text(
-            "Welcome to the Explore",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 34,
-              fontWeight: FontWeight.bold,
-              color: Color(0xFF003366),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+            decoration: BoxDecoration(
+              color: Colors.grey[100],
+              borderRadius: BorderRadius.circular(12),
+              border: Border.all(
+                color: Theme.of(context).primaryColor,
+                width: 1
+              )
+            ),
+            child: Row(
+              children: [
+                Icon(Icons.search, color: theme.primaryColor),
+                const SizedBox(width: 10),
+                const Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Search property',
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.tune, color: theme.primaryColor),
+                  onPressed: () {
+                  },
+                ),
+              ],
             ),
           ),
         ),
