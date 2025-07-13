cc87e3ff242d8c722d12d4cb3a21b540a406d55c
bhumi3292 <137974891+bhumi3292@users.noreply.github.com>
2025-07-13 18:50:51 +0545

    category add

--- dream_dwell/lib/app/constant/hive_table_const.dart
+++ dream_dwell/lib/app/constant/hive_table_const.dart
@@ -7,5 +7,7 @@ class HiveTableConstant {
 
   static const int propertyTableId = 1;
   static const String propertyBox = 'propertyBox';
-}
 
+  static const int categoryTableId = 2;
+  static const String categoryBox = 'categoryBox';
+}
