fa6eff846266f0a6c3704ef1ee8ee4107654d083
bhumi3292 <137974891+bhumi3292@users.noreply.github.com>
2025-07-22 21:34:31 +0545

    Remove dreamdwell_frontend and dreamdwell_backend directories

--- dreamdwell_backend/utils/asyncHandler.js
+++
@@ -1,8 +0,0 @@
-// dreamdwell_backend/utils/asyncHandler.js.
-const asyncHandler = (requestHandler) => {
-    return (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
-    };
-};
-
-module.exports = { asyncHandler };
--- dreamdwell_frontend/src/api/chatbotApi.js
+++
@@ -1,5 +0,0 @@
-// src/api/chatbotApi.js
-import instance from "./api";
-export const sendChatQueryApi = (data) => {
-    return instance.post("/api/chatbot/query", data);
-};
